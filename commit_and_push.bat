b17ef7bdb0001c70543e58e3f315d4dfd65ce337
Alex Chomba <chombaalex2019@gmail.com>
2025-10-20 23:43:45 +0300

    Add deployment guides

---
+++ commit_and_push.bat
@@ -0,0 +1,5 @@
+@echo off
+git commit -m "Add Railway and Vercel deployment configuration"
+git push origin main
+echo Deployment files pushed to GitHub!
+pause
---
+++ final_push.bat
@@ -0,0 +1,6 @@
+@echo off
+git add .
+git commit -m "Add deployment guides"
+git push origin main
+echo Done!
+pause
